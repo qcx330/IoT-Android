cd18fedc45d71e57b0275efaf65772b697a21b0c
Quỳnh <79866567+qcx330@users.noreply.github.com>
2023-10-19 21:14:20 +0700

    update

--- app/src/main/java/com/example/test/AdafruitConnect.java
+++ app/src/main/java/com/example/test/AdafruitConnect.java
@@ -28,6 +28,7 @@ public class AdafruitConnect {
     public final String[] topic = {"qcx330/feeds/nutnhan1", "qcx330/feeds/nutnhan2"};
     private static final String BROKER_URI = "tcp://io.adafruit.com:1883";
     private static final String USERNAME = "qcx330";
+    //Thay đổi password theo hướng dẫn
     private static final String PASSWORD = "aio_yRJc76ZnjvAXYj9ZJCU6Kna5sdHL";
     Random random = new Random();
     final String clientId = "publish-" + random.nextInt(1000) + 1;
